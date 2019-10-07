df42359011056f789798786fe15ff8042ed1b184
FredericDT <i@fdt.onl>
2019-10-07 17:44:21 +0800

    Commit analytical information - move error info to stderr

--- src/lexical_parser_result_analysis.cpp
+++ src/lexical_parser_result_analysis.cpp
@@ -2,4 +2,69 @@
 // Created by FredericDT on 10/7/2019.
 //
 
+#include <sstream>
 #include "lexical_parser_result_analysis.h"
+
+namespace fdt {
+    namespace lexical_parser {
+
+        fdt::lexical_parser::analysis_result::analysis_result() : total_char_count(-1), total_line_count(-1),
+                                                                  lexical_element_type_count{0} {}
+
+        long fdt::lexical_parser::analysis_result::get_total_line_count() const {
+            return total_line_count;
+        }
+
+        fdt::lexical_parser::analysis_result &
+        fdt::lexical_parser::analysis_result::set_total_line_count(long total_line_count) {
+            analysis_result::total_line_count = total_line_count;
+            return *this;
+        }
+
+        long fdt::lexical_parser::analysis_result::get_total_char_count() const {
+            return total_char_count;
+        }
+
+        fdt::lexical_parser::analysis_result &
+        fdt::lexical_parser::analysis_result::set_total_char_count(long total_char_count) {
+            analysis_result::total_char_count = total_char_count;
+            return *this;
+        }
+
+        const long *fdt::lexical_parser::analysis_result::get_lexical_element_type_count() const {
+            return lexical_element_type_count;
+        }
+
+        std::string fdt::lexical_parser::analysis_result::to_string() {
+            std::stringstream ss = std::stringstream();
+            ss << "total lines: " << total_line_count << std::endl;
+            ss << "total characters: " << total_char_count << std::endl;
+            ss << "tokens count: " << std::endl;
+            for (int i = 0; i < lexical_element::TYPE_ENUM_LENGTH; ++i) {
+                ss << "\t" << lexical_parser::to_string(static_cast<enum lexical_element::type >(i)) << ": " << lexical_element_type_count[i] << std::endl;
+            }
+
+            return std::string(ss.str());
+        }
+
+        void analysis_result::print_to_ostream(std::ostream &stream) {
+            stream << this->to_string();
+        }
+
+        fdt::lexical_parser::analysis_result
+        analysis_result::build_from_lexical_parse_result(fdt::lexical_parser::lexical_parse_result &v) {
+            analysis_result analysis = analysis_result();
+
+            analysis.set_total_char_count(v.get_total_chars());
+            analysis.set_total_line_count(v.get_total_lines());
+
+            const std::vector<lexical_element> &lexical_element_vector = v.get_lexical_element_vector();
+            for (auto i = lexical_element_vector.begin(); i != lexical_element_vector.end(); ++i) {
+                ++analysis.lexical_element_type_count[i->get_type()];
+            }
+
+            return analysis;
+        }
+
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 #include "cxxopts.hpp"
 #include "arguments_parser.h"
 #include "file_check.h"
+#include "lexical_parser_result_analysis.h"
 
 int main(int argc, char *argv[]) {
+
     fdt::lexical_parser::parse_result arguments_parse_result = fdt::lexical_parser::parse_arguments(argc, argv);
 
     std::ifstream input_file = std::ifstream(arguments_parse_result.file_path);
@@ -16,19 +18,30 @@ int main(int argc, char *argv[]) {
                                                                                                         arguments_parse_result.verbose);
     bool output_file_provided = !arguments_parse_result.output_file.empty();
 
-    if (output_file_provided) {
-        std::ofstream output_file = std::ofstream(arguments_parse_result.output_file);
-        fdt::lexical_parser::check_file_good_or_exit_error(output_file, arguments_parse_result.output_file);
+    if (lexical_parse_result.is_exception_occur()) {
         fdt::lexical_parser::output_lexical_parse_result_to_output_stream(
                 lexical_parse_result,
-                output_file);
-        output_file.close();
+                std::cerr);
     } else {
-        fdt::lexical_parser::output_lexical_parse_result_to_output_stream(
-                lexical_parse_result,
-                std::cout);
+        if (output_file_provided) {
+            std::ofstream output_file = std::ofstream(arguments_parse_result.output_file);
+            fdt::lexical_parser::check_file_good_or_exit_error(output_file, arguments_parse_result.output_file);
+            fdt::lexical_parser::output_lexical_parse_result_to_output_stream(
+                    lexical_parse_result,
+                    output_file);
+            output_file.close();
+        } else {
+            fdt::lexical_parser::output_lexical_parse_result_to_output_stream(
+                    lexical_parse_result,
+                    std::cout);
+        }
     }
 
+    if (arguments_parse_result.analysis) {
+        fdt::lexical_parser::analysis_result analysis_result = fdt::lexical_parser::analysis_result::build_from_lexical_parse_result(
+                lexical_parse_result);
+        analysis_result.print_to_ostream(std::cout);
+    }
 
-    return 0;
+    return !lexical_parse_result.is_exception_occur();
 }
